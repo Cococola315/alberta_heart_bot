cbd0cbf06d13e1cab94defa95fd359b1bed2cc79
Cohen Chan <cmchan@ualberta.ca>
2025-10-30 16:52:36 -0600

    Added webhook link to record bot usage

--- bot.py
+++ bot.py
@@ -3,11 +3,13 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from openai import OpenAI
+import requests
 
 # Load keys
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 CHATGPT_API_KEY = os.getenv("CHATGPT_API_KEY")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 # Setup instance of OpenAI client
 client_ai = OpenAI(api_key=CHATGPT_API_KEY)
@@ -18,12 +20,27 @@ intents.messages = True
 intents.message_content = True  # required to read message text
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+def send_to_webhook(message:str):
+    payload = {
+        "content": message,
+        "username": "ChatGPT Logger"
+    }
+    requests.post(WEBHOOK_URL,json=payload)
+
 @bot.event
 async def on_ready():
     print(f"🤖 Logged in as {bot.user}")
 
 @bot.command(name="ask")
 async def ask(ctx, *, prompt: str):
+
+    # Only respond in the allowed channel
+    allowed_channel_id = 1418946574737866933
+
+    if ctx.channel.id != allowed_channel_id:
+        return # do nothing if the command was used outside allowed channel
+    
+
     """Ask ChatGPT a question with !ask <question>"""
     await ctx.send("Thinking...")
 
@@ -37,5 +54,7 @@ async def ask(ctx, *, prompt: str):
 
     answer = response.choices[0].message.content
     await ctx.send(answer[:1900])  # Discord message limit is 2000 chars
+
+    send_to_webhook(f"**{ctx.author} asked:** {prompt}\n**ChatGPT replied** {answer[:1900]}")
 
 bot.run(DISCORD_TOKEN)
