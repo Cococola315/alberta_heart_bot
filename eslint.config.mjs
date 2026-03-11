c017a6f16e1f638c5520f1af3430e6648b56d5b3
amongus <adi4soni@gmail.com>
2026-03-10 23:44:28 -0600

    added husky for auto formatting before committing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    ignores: ['dist/**', 'node_modules/**', 'build/**']
+  },
+  { 
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], 
+    plugins: { js }, 
+    extends: ['js/recommended'], 
+    languageOptions: { globals: globals.node } 
+  },
+  { 
+    files: ['**/*.js'], 
+    languageOptions: { sourceType: 'commonjs' } 
+  },
+  {
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'indent': ['error', 2],
+      'camelcase': 'warn',
+      'eqeqeq': 'warn',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+    }
+  }
 ]);
